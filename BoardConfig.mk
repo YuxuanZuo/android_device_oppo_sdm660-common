9b64879392fe3c7c9462865345ba3f7657df909f
Himanshu Agrawal <hagraw@codeaurora.org>
2019-03-27 14:35:58 +0530

    sdm660_64: AOSP complaint compilation changes.
    - Set correct CROSS_COMPILER_VERSION for 64-bit variant.
    - Disable oem-services bootjar.
    
    Change-Id: I9adf3c5fda00e3756cf30257b36300e18fbb997d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,7 +122,7 @@ BOARD_RAMDISK_OFFSET     := 0x02000000
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 #TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
 TARGET_USES_UNCOMPRESSED_KERNEL := false
 
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
--- sdm660_64.mk
+++ sdm660_64.mk
@@ -123,7 +123,7 @@ PRODUCT_PACKAGES += telephony-ext
 ifneq ($(strip $(QCPATH)),)
 #PRODUCT_BOOT_JARS += WfdCommon
 #Android oem shutdown hook
-PRODUCT_BOOT_JARS += oem-services
+#PRODUCT_BOOT_JARS += oem-services
 endif
 
 # system prop for Bluetooth SOC type
