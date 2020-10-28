3419b2e4b919a3e1623f12d913a920f10bfdbfc3
Vinoth Jayaram <c_vinoja@codeaurora.org>
2020-10-28 13:17:57 +0530

    sdm660: Updated Light HAL
    Enabled Light HAL based on API level.
    
    Change-Id: Ib4c56493e57e3a473f12265ff889c57e53786259

--- sdm660_64.mk
+++ sdm660_64.mk
@@ -266,10 +266,18 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
     android.hardware.configstore@1.0-service \
     android.hardware.broadcastradio@1.0-impl
+
+ifeq (true,$(call math_gt_or_eq,$(SHIPPING_API_LEVEL),29))
+#Enable Light AIDL HAL
+PRODUCT_PACKAGES += android.hardware.lights-service.qti
+else
+#Enable Light HIDL HAL
+PRODUCT_PACKAGES += \
+android.hardware.light@2.0-impl \
+android.hardware.light@2.0-service
+endif
 
 PRODUCT_PACKAGES += \
     vendor.display.color@1.0-service \
