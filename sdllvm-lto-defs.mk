24f61e846141ebbb3bc322128bad1e37c5420071
YSR <ysr@codeaurora.org>
2021-03-29 20:27:15 -0700

    common:Update 64bit only build configuration
    Change-Id: I127749a68c5cf76aa83dfbb0a39cf21aad41bc88

--- sdllvm-lto-defs.mk
+++ sdllvm-lto-defs.mk
@@ -14,7 +14,9 @@ $(LOCAL_BUILT_MODULE) : $(combo_2nd_arch_prefix)TARGET_AR := $(AR)
 $(LOCAL_BUILT_MODULE) : $(combo_var_prefix)GLOBAL_ARFLAGS := $(ARFLAGS)
 
 # For 64 bit
+ifdef TARGET_2ND_ARCH
 intermediates := $(call local-intermediates-dir,,$(LOCAL_2ND_ARCH_VAR_PREFIX))
+endif
 LOCAL_BUILT_MODULE_64 := $(intermediates)/$(my_installed_module_stem)
 
 $(LOCAL_BUILT_MODULE_64) : TARGET_AR := $(AR)
@@ -26,7 +28,9 @@ else
 
 FLAGS_TO_BE_FILTERED := -Wl,--icf=safe -Wl,--no-undefined-version -Wl,--fix-cortex-a53-843419 -fuse-ld=gold -Wl,-m,aarch64_elf64_le_vec
 linked_module_32 := $(intermediates)/LINKED/$(my_installed_module_stem)
+ifdef TARGET_2ND_ARCH
 intermediates    := $(call local-intermediates-dir,,$(LOCAL_2ND_ARCH_VAR_PREFIX))
+endif
 linked_module_64 := $(intermediates)/LINKED/$(my_installed_module_stem)
 
 $(linked_module_32) : PRIVATE_TARGET_GLOBAL_LDFLAGS := $(filter-out $(FLAGS_TO_BE_FILTERED),$(PRIVATE_TARGET_GLOBAL_LDFLAGS))
