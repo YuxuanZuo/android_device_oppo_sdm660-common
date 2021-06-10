f8471cf6526a6c9a811b88eb878c7b3e4b8a48e8
Phalguni Bumhyavarapu <pbumhyav@codeaurora.org>
2021-06-10 14:17:09 -0700

    device/qcom/common/base.mk: Remove DRM Widevine and Clearkey HALs
    DRM Widevine and Clearkey HAL entries moved securemsm specific
    makefiles complying FR53051.
    
    Change-Id: I61659cc305d6c5e129a7b134697441c3c312e297

--- base.mk
+++ base.mk
@@ -1174,9 +1174,6 @@ PRODUCT_PACKAGES += libqti_vndfwk_detect_vendor
 PRODUCT_PACKAGES += libvndfwk_detect_jni.qti_system
 PRODUCT_PACKAGES += libvndfwk_detect_jni.qti_vendor
 
-PRODUCT_PACKAGES += android.hardware.drm@1.3-service.widevine
-PRODUCT_PACKAGES += android.hardware.drm@1.3-service.clearkey
-
 #soong namespace for qssi vs vendor differentiation
 SOONG_CONFIG_NAMESPACES += qssi_vs_vendor
 SOONG_CONFIG_qssi_vs_vendor += qssi_or_vendor
