4d6aa9b1be58917fbe9a177505ec91e8229fed11
Ethan Zuo <yuxuan.zuo@outlook.com>
2021-11-04 01:20:12 +0800

    sdm660-common: init: Remove TWRP default usb init script

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -118,6 +118,7 @@ TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_INCLUDE_FB2PNG := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_INCLUDE_NTFS_3G := true
 TW_OZIP_DECRYPT_KEY := D7DBCE1AD4AFDCE1393E5121CBDC4321
 TW_EXTRA_LANGUAGES := true
